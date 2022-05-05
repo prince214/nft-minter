338b360038220db156ef2ba711f86a10e14f2929
prince214 <princeparaste78@gmail.com>
2022-05-05 22:23:08 +0530

    ui: change header title

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -116,6 +116,7 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
+        <p className="header gradient-text">NFT Builder</p>
           <p className="header gradient-text">My NFT Collection</p>
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
